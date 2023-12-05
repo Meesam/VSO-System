e5083f5c9e4e37b902e469858604f2bf3735269e
Meesam <meesam.engineer@gmail.com>
2023-12-05 19:19:57 +0530

    add sidebar items and style with tailwind css

---
+++ VSO-SystemApp/Data/SideBar.cs
@@ -0,0 +1,16 @@
+﻿namespace VSO_SystemApp.Data
+{
+    public class SideBar
+    {
+       public string? Title { get; set; }
+       public string? Url { get; set; }
+       public string? Icon { get; set; }
+
+       public SideBar(string title, string url, string icon) 
+       {
+            Title = title;
+            Url = url;
+            Icon = icon;
+       }
+    }
+}
