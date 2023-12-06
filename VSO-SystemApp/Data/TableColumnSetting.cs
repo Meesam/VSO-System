b7b7aa9c87ba57766bcacc76cac9508b02e287e2
Meesam <meesam.engineer@gmail.com>
2023-12-06 19:19:18 +0530

    add Table in pages

---
+++ VSO-SystemApp/Data/TableColumnSetting.cs
@@ -0,0 +1,14 @@
+﻿namespace VSO_SystemApp.Data
+{
+    public class TableColumnSetting
+    {
+        public string Title { get; set; }
+        public bool Sortable { get; set; }
+
+        public TableColumnSetting(string title, bool sortable) 
+        {
+           Title = title;
+           Sortable = sortable;
+        }
+    }
+}
