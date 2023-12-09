3b65e014a1f79d971a55069b2048440dcf1a8715
Meesam <meesam.engineer@gmail.com>
2023-12-09 09:26:29 +0530

    add new logo and Home page design complete

--- VSO-SystemApp/tailwind.config.js
+++ VSO-SystemApp/tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ['./**/*.{razor, html}'],
   theme: {
-    extend: {},
+      screens: {
+          sm: '480px',
+          md: '768px',
+          lg: '976px',
+          xl: '1440px',
+      },
+      extend: {},
   },
   plugins: [],
 }
